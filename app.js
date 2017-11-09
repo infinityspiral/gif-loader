12ef3cf98466c915c9e3e320db5cf1185c93fb2c
CN58090 <NortonChristopherP@johndeere.com>
2017-11-09 12:20:32 -0600

    add loading indicator and format results

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 const API_KEY = 'oMyHno2Mn152yopJ2mS9cC2rDDC1dENf';
-const BASE_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=5`;
+const BASE_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}`;
 
 const gifForm = document.querySelector('#gif-form');
+const gifLimit = gifForm.querySelector('#gif-search-limit');
 const gifInput = gifForm.querySelector('#gif-search-input');
 gifInput.focus();
 
 const gifList = document.querySelector('#gif-list');
 
 const searchGiphy = (inputText = 'test') => {
-  return fetch(`${BASE_URL}&q=${inputText}`)
+  return fetch(`${BASE_URL}&limit=${gifLimit.value}&q=${inputText}`)
   .catch(e => {
     console.log('not found');
   })
   .then(res => res.json())
-  .then(data => data.data, (e) => {
-    console.log(e);
-  })
+  .then(data => data.data)
 }
 
 const createFields = (data) => {
   gifList.innerHTML = '';
+  console.log(data);
 
   if (data.length){
     data.map(gifItem => {
       const gifContainer = document.createElement('div');
-      const gifImage = document.createElement('img');
       const gifText = document.createElement('p');
+      const gifImage = document.createElement('img');
 
       gifContainer.className = 'gif-container';
+      gifImage.addEventListener('load', (e) => {
+        //set an attribute that css can hook into
+        gifContainer.setAttribute('data-loaded', true);
+      })
       gifImage.src = gifItem.images.fixed_width.url;
       gifText.innerText = gifItem.images.fixed_width.url;
-
       gifContainer.appendChild(gifImage);
       gifContainer.appendChild(gifText);
-
       gifList.appendChild(gifContainer);
     });
   } else {
@@ -46,6 +48,7 @@ const createFields = (data) => {
 
 gifForm.addEventListener('submit', (e)=> {
   e.preventDefault();
+  gifInput.blur();
 
   const inputText = gifInput.value;
   searchGiphy(inputText)
